3cd036ff667d2e816eb56f5d44b53a466c91fe87
Nat-Des <123428679+Nat-Des@users.noreply.github.com>
2023-05-02 16:14:32 -0500

    Initial commit

---
+++ src/test/java/com/saracks/storeweb/StoreWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saracks.storeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
